5d2764f3f26270ca1bad5d91a04a8718da54a422
Glitch (challenge-3-4) <none>
2018-03-03 05:54:42 +0000

    🚀🎌 Checkpoint index.html:383374/715 script.js:383374/455

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
-// This line (which starts with two forward slashes) is what we call a comment!
-// Comments are ignored by the computer, and they're very handy for writing notes to yourself!
+// Remember to compare THREE things to understand what this code does:
+//    1) this file (the JavaScript code)
+//    2) the HTML file
+//    3) the result that you see in the live web page (click "Show Live" in Glitch to see the actual web page)
 
-// You can use comments for short explanations of what your code does,
-//    or to leave yourself reminders ("TODO: fix this bug on line 27!"),
-//    or to include ASCII art and silly emojis.   ⊂(◉‿◉)つ     ヽ༼ ಠ益ಠ ༽ﾉ     ¯\_(ツ)_/¯
-
-// You can also "comment out" lines of code to keep old versions of code handy as a reference,
-//     without having the computer actual run the code. This is a really useful trick! :)
-// For example, here's some broken code saved inside some comments:
-//     console.log(blablah);
-//     console.log(stuff in here);
-
-// **Important tip:**
-// You can "comment out" a bunch of code all at once by selecting multiple lines of code,
-//    and then pressing Ctrl + / on Windows or Cmd + / on Mac.
-//    You can repeat the process to "uncomment" lines of code too.
-
-console.log("This line of code displays a message (this message!!) in the browser console. Hello, world!");
+document.body.textContent = "This JS code will replace all the text of the body element in the HTML file!";
