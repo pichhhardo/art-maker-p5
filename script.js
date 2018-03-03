62bed7fc2682255134ffae40c2d4f068e6914734
Glitch (challenge-1-2) <none>
2018-03-03 05:42:11 +0000

    🌄🚆 Checkpoint script.js:383374/270 index.html:383374/23

--- script.js
+++ script.js
@@ -5,13 +5,15 @@
 //    or to leave yourself reminders ("TODO: fix this bug on line 27!"),
 //    or to include ASCII art and silly emojis.   ⊂(◉‿◉)つ     ヽ༼ ಠ益ಠ ༽ﾉ     ¯\_(ツ)_/¯
 
-// You can also "comment out" lines of code temporarily to keep old versions of code around,
-// without having the computer actual run the code. 
+// You can also "comment out" lines of code to keep old versions of code handy as a reference,
+//     without having the computer actual run the code. This is a really useful trick! :)
 // For example, here's some broken code saved inside some comments:
-// console.log(blablah);
-// console.log(stuff in here);
+//     console.log(blablah);
+//     console.log(stuff in here);
 
 // **Important tip:**
-// You can "comment out" a bunch of code all at once by selecting multiple lines of code and then pressing `Ctrl + /` on Windows or `Cmd + /` on Mac. You can repeat the process to *uncomment* lines of code too.
+// You can "comment out" a bunch of code all at once by selecting multiple lines of code,
+//    and then pressing Ctrl + / on Windows or Cmd + / on Mac.
+//    You can repeat the process to "uncomment" lines of code too.
 
 console.log("This line of code displays a message (this message!!) in the browser console. Hello, world!");
