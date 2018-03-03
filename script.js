2933c6905f33dedc55bebb6fdbcfc81a8807b3ed
Glitch (hospitable-coat) <none>
2018-03-03 05:10:29 +0000

    💾👲 Checkpoint script.js:383374/1041 index.html:383374/599

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("This line of code displays a message (this message!!) in the browser console. Hello ");
+// This line (which starts with two forward slashes) is what we call a comment!
+// Comments are ignored by the computer, and they're very handy for writing notes to yourself!
+
+// You can use comments for short explanations of what your code does,
+//    or to leave yourself reminders ("TODO: fix this bug on line 27!"),
+//    or to include ASCII art and silly emojis.   ⊂(◉‿◉)つ     ヽ༼ ಠ益ಠ ༽ﾉ     ¯\_(ツ)_/¯
+
+console.log("This line of code displays a message (this message!!) in the browser console. Hello, world!");
