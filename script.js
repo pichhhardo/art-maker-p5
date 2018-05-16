ff558e7529574b5ee133c23b55687406b7e4e338
Glitch (js-challenge-1-2) <none>
2018-05-16 19:10:16 +0000

    💎📠 Checkpoint ./script.js:383374/1117 ./index.html:383374/259 ./styles.css:383374/92

--- script.js
+++ script.js
@@ -3,4 +3,5 @@
 //    2) the HTML file
 //    3) the result that you see in the live web page (click "Show Live" in Glitch to see the actual web page)
 
+
 document.body.textContent = "This JS code will replace all the text of the body element in the HTML file!";
