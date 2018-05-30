4a3668c728a21237d6b0512d049c6298bef68211
Glitch (js-group-challenge-12) <none>
2018-05-30 23:30:36 +0000

    😺🌼 Checkpoint ./script.js:383374/242

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-// Let's write some code together!
+//    NOTE: these lines that start with two slashes are called COMMENTS.
+//    Comments are ignored by the computer; they're great for writing notes to yourself!
 
+// Let's write some code together!
 
