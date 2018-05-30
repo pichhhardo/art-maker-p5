c4559ea1a44826e16da603779036ff8d4807de31
Glitch (js-group-challenge-12) <none>
2018-05-30 23:51:04 +0000

    ☝️🗼 Checkpoint ./script.js:383374/143

--- script.js
+++ script.js
@@ -2,4 +2,7 @@
 //    Comments are ignored by the computer; they're great for writing notes to yourself!
 
 // Let's write some code together!
+//    Instructions for section 1:
+//    https://github.com/LearnTeachCode/beginner-javascript-workshop/blob/master/1-variable-challenges.md
+
 
