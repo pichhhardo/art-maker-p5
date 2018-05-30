ecfbf2d565aa7c2ce387e93da0f1534b6410fa07
Glitch (js-group-challenge-12) <none>
2018-05-30 23:20:04 +0000

    ⛄️🗺 Checkpoint ./index.html:383374/274 ./script.js:383374/560

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// Remember to compare THREE things to understand what this code does:
-//    1) this file (the JavaScript code)
-//    2) the HTML file
-//    3) the result that you see in the live web page (click "Show Live" in Glitch to see the actual web page)
+// Let's write some code together!
 
 
-// Our code in the last challenge looked like this:
-// document.body.textContent = "This JS code will replace ALL the text in the body element of the HTML file!";
-
-
-document.getElementById("special").textContent = "This JS code will replace all the text inside just ONE paragraph.";
