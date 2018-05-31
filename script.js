84d832ca6574f7c478125d4ff4d15af73a8df8ad
Glitch (js-group-challenge-12) <none>
2018-05-31 02:40:58 +0000

    🙇👨‍❤️‍👨 Checkpoint ./script.js:383374/20:751900/28:1231284/35:1231286/52:1231288/53:1231300/63:1231302/97:1231315/42

--- script.js
+++ script.js
@@ -7,15 +7,36 @@
 
 
 // Create a new variable
-let myNerdyJoke; = "Debugging is like being the detective in a crime drama where you are also the murderer";
+let myNerdyJoke = "Debugging is like being the detective in a crime drama where you are also the murderer";
 
 // Display it in the console
 console.log(myNerdyJoke);
 
 
+let liz = "hellloo, I love JavaScript and hope you do too!";
+console.log(liz);
+
+let stefon = "From NC and is a huge Tar Heels fan" 
+console.log(stefon);
 
 
 
+let ali = "from Istanbul" + 5;
+console.log(ali);
 
 
+let gabe = aloha
+console.log(gabe)
 
+pauline = = test
+console.log(pauline);
+
+let jason = "JavaScript"
+jason = 2018
+console.log(jason)
+
+
+let kat = "Hi";
+console.log(kat);
+
+let andy = "Hello world!";
