1ad08cfc6353c806cc57bac40729adf57fdefd2a
Glitch (js-group-challenge-12) <none>
2018-05-31 02:23:34 +0000

    😋🖼 Checkpoint ./script.js:383374/401:751900/12:1231284/7:1231286/20:1231288/22:1231300/4:1231302/24:1231315/14

--- script.js
+++ script.js
@@ -6,3 +6,16 @@
 //    https://github.com/LearnTeachCode/beginner-javascript-workshop/blob/master/1-variable-challenges.md
 
 
+// Create a new variable
+let myNerdyJoke; = "Debugging is like being the detective in a crime drama where you are also the murderer";
+
+// Display it in the console
+console.log(myNerdyJoke);
+
+
+
+
+
+
+
+
