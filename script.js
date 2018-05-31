1bd1e7e9ebd651a0a8329531f66261666ce08aba
Glitch (js-group-challenge-12) <none>
2018-05-31 02:51:11 +0000

    🏟🌿 Checkpoint ./script.js:383374/119:1231286/49:1231288/24:1231300/4:1231315/1

--- script.js
+++ script.js
@@ -6,37 +6,39 @@
 //    https://github.com/LearnTeachCode/beginner-javascript-workshop/blob/master/1-variable-challenges.md
 
 
-// Create a new variable
-let myNerdyJoke = "Debugging is like being the detective in a crime drama where you are also the murderer";
+// // Create a new variable
+// let myNerdyJoke = "Debugging is like being the detective in a crime drama where you are also the murderer";
 
-// Display it in the console
-console.log(myNerdyJoke);
+// // Display it in the console
+// console.log(myNerdyJoke);
 
+// let liz ="hellloo, I love JavaScript and hope you do too!";
+// liz = "new value";
+// console.log(liz);
 
-let liz = "hellloo, I love JavaScript and hope you do too!";
-console.log(liz);
+// let stefon = "From NC and is a huge Tar Heels fan"
+// console.log(stefon);
 
-let stefon = "From NC and is a huge Tar Heels fan" 
-console.log(stefon);
-
-
-
-let ali = "from Istanbul" + 5;
-console.log(ali);
-
-
-let gabe = aloha
-console.log(gabe)
-
-pauline = = test
-console.log(pauline);
-
-let jason = "JavaScript"
-jason = 2018
-console.log(jason)
+// let aloha = "test"
+// let ali = "from Istanbul" + 5;
+// console.log(ali);
 
 
-let kat = "Hi";
-console.log(kat);
+// let gabe = aloha;
+// console.log(gabe);
 
-let andy = "Hello world!";
+// //pauline = = test
+// //console.log(pauline);
+
+// let jason = "JavaScript"
+// jason = 2018 / 0
+// console.log(jason)
+
+
+// let kat = "Hi";
+
+// console.log(kat);
+
+// let andy = "Hello world!";
+// console.log(andy);
+
