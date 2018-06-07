9036421986f49e73e18cfb9f8ef30dbc8385492d
Glitch (js-group-challenge-12) <none>
2018-06-07 01:08:04 +0000

    ⛹🦂 Checkpoint ./script.js:383374/1066

--- script.js
+++ script.js
@@ -2,43 +2,18 @@
 //    Comments are ignored by the computer; they're great for writing notes to yourself!
 
 // Let's write some code together!
-//    Instructions for section 1:
-//    https://github.com/LearnTeachCode/beginner-javascript-workshop/blob/master/1-variable-challenges.md
+
+// EXAMPLE CODE TO COPY-PASTE -- introduce yourself! :)
+
+console.log("Hi, I'm Liz! I do freelance web dev, teach coding classes, and run meetup groups. :)");
 
 
-// // Create a new variable
-// let myNerdyJoke = "Debugging is like being the detective in a crime drama where you are also the murderer";
-
-// // Display it in the console
-// console.log(myNerdyJoke);
-
-// let liz ="hellloo, I love JavaScript and hope you do too!";
-// liz = "new value";
-// console.log(liz);
-
-// let stefon = "From NC and is a huge Tar Heels fan"
-// console.log(stefon);
-
-// let aloha = "test"
-// let ali = "from Istanbul" + 5;
-// console.log(ali);
 
 
-// let gabe = aloha;
-// console.log(gabe);
-
-// //pauline = = test
-// //console.log(pauline);
-
-// let jason = "JavaScript"
-// jason = 2018 / 0
-// console.log(jason)
 
 
-// let kat = "Hi";
 
-// console.log(kat);
 
-// let andy = "Hello world!";
-// console.log(andy);
+
+
 
