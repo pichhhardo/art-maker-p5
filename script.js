54ded7ac4a700b2d29ef836ea81d1f165793d4a0
Glitch (js-group-challenge-12) <none>
2018-06-07 02:23:51 +0000

    💋👓 Checkpoint ./script.js:383374/211:697530/130:1135685/274:1272304/248:1272308/261:1272309/638:1272318/192

--- script.js
+++ script.js
@@ -7,13 +7,25 @@
 
 console.log("Hi, I'm Liz! I do freelance web dev, teach coding classes, and run meetup groups. :)");
 
+console.log("Howdy, I'm Steve.! I actually hold the title of Software Developer and it kind of scares me!");
+
+console.log("Hi, I'm June! I'm a technical writer trying to get more familiar with code.");
 
 
+  Me too - June
 
 
+dfgdfg
 
 
+hi!
+dfgdfg
 
 
+console.log("Hi, I'm Ruby! I'm a product designer, artist, and earth lover. :)");
 
+console.log("Hi! I am Marla  I am a film editor wanting to learn something new!  Have web app ideas to pursue  :-) ;
 
+console.log("Hi I'm Nicole! I want to make fun interactive webpages");
+
+sole.log("Hi, I'm Carmen! I'm new to coding and wish to learn more!. :)");
