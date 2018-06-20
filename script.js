b9394b6f9c38f7ff3773f7c52197832bcc38f428
Glitch (js-group-challenge-12) <none>
2018-06-20 23:24:56 +0000

    💗🎌 Checkpoint ./script.js:383374/581

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 //    NOTE: these lines that start with two slashes are called COMMENTS.
 //    Comments are ignored by the computer; they're great for writing notes to yourself!
 
-// Let's draw some stuff by writing some code together!
 
 
 // The code inside the setup function will run ONLY ONCE when you open the web page
@@ -10,17 +9,27 @@ function setup() {
   // The createCanvas function creates a <canvas> element on the page after it loads, which lets us draw stuff!
   createCanvas(500, 500);
   
+  
+  // EXAMPLE CODE -- Draw a square and then draw the text "Liz"
+  rect(100, 30, 50, 50);
+  text("Liz", 100, 30);
 
-
+  // Copy the two lines of example code above, then paste it below, and change it to say your name!
+  
+  
+  
+  
+  
+  
   
 }
 
 
-// The code inside the draw function will run OVER AND OVER again (so later, we can animate!)
+// The code inside the draw function will run OVER AND OVER again
 function draw() {
+    
+  // When we animate our drawings, we'll do this in here!
   
-  rect(100, 30, 50, 50);
-  text("Liz", 100, 30);
   
 }
 
