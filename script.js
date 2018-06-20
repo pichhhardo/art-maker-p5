ce38716a74b82fe9be1f79ba69167cd61d76f316
Glitch (js-group-challenge-12) <none>
2018-06-20 23:04:00 +0000

    👮🍈 Checkpoint ./index.html:383374/208 ./script.js:383374/404

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
 //    NOTE: these lines that start with two slashes are called COMMENTS.
 //    Comments are ignored by the computer; they're great for writing notes to yourself!
 
-// Let's write some code together!
+// Let's write some code together 
 
-// EXAMPLE CODE TO COPY-PASTE -- introduce yourself! :)
-
-console.log("Liz - hi!  ");
-
-console.log("ali - ux designer - from Istanbul - Novice Coder")
-
-console.log("blair - hey!");
-
-console.log("Meg - hello!");
-
-console.log ("June - Hiyeee!);
-
-console.log("Kim, here and present.");
-
-console.log("Hola everyone I am OMAR");
-
-console.log ("xio- hello!!!!");
-
-             
-             
-             
