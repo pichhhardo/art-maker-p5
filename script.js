102aa272dbad681c651b9473299357687c2512d8
Glitch (canvas-challenges) <none>
2018-06-21 00:24:22 +0000

    🐛📯 Checkpoint ./script.js:383374/1100

--- script.js
+++ script.js
@@ -2,31 +2,34 @@
 //    Comments are ignored by the computer; they're great for writing notes to yourself!
 
 
-let squareWidt
+// Creating the variables OUTSIDE of any function makes sure we can access them ANYWHERE in our file. (Called GLOBAL variables)
+let squareWidth = 75;
 let xPos = 0;
 let yPos = 0;
+
 
 // The code inside the setup function will run ONLY ONCE when you open the web page
 function setup() {
   
   // The createCanvas function creates a <canvas> element on the page after it loads, which lets us draw stuff!
   createCanvas(500, 500);
-  
-  
-  // Create some drawings right here! :)
-  
-  
-
-  rect(400, 350, squareWidth, squareWidth);
-  
+   
 }
 
 
 // The code inside the draw function will run OVER AND OVER again
 function draw() {
     
-  // When we animate our drawings, we'll do it in here!
+  // Let's animate! To make shapes MOVE, we need to now use our shape functions in here instead.
   
+  // EXAMPLE:
+  // Step 1: draw a blue square on EVERY frame, using our variables
+  fill("blue");
+  rect(xPos, yPos, squareWidth, squareWidth);
+  
+  // Step 2: Change the xPos to become equal to its previous value, plus 10
+  //        So on every animation frame, the square is redrawn at 10 pixels past its previous location
+  xPos = xPos + 10;
   
 }
 
