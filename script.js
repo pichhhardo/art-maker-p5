f84924bdd5f701e955a97d5938cfe35063861189
Glitch (p5js-sound1) <none>
2018-08-05 15:35:17 +0000

    🌂💰 Checkpoint ./script.js:383374/1083

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
 console.log("if you see this message in the console, then we know this JavaScript file is running!");
 
+
+// Create a new oscillator -- this lets us synthesize our own sounds!
+let osc = new p5.Oscillator();
+
+
 // The code inside the setup function will run ONLY ONCE when you open the web page
 function setup() {
   
-  // The createCanvas function creates a <canvas> element on the page after it loads, which lets us draw stuff!
+  // Setup code from before, for drawings/animations:
   createCanvas(500, 500);
-  
-  // Fix for retina displays where clear() only clears the top left corner of the canvas)
   pixelDensity(1);
-
-  // 60 frames per second
   frameRate(60);
     
-  ////////////////////////////////////////////////////////////////////////////////////////////////
-  // Include your code here inside the setup() function
-  // to do something once when the page loads.
-  ////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-  // Create a new oscillator -- this lets us synthesize our own sounds!
-  let osc = new p5.Oscillator();
-  
   // Set the type of sound wave
   osc.setType('sine');
   
@@ -38,6 +31,11 @@ function setup() {
 }  // end of setup() function
 
 
+// This p5js function runs when the user clicks their mouse
+function mouseClicked() {
+  // Stop playing the sound!
+  osc.stop();
+}
 
 
 // The code inside the draw function will run OVER AND OVER again
