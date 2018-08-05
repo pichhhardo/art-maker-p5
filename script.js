58b175cd623ced8bcb4e4b8572f112533256cff2
Glitch (p5js-sound3) <none>
2018-08-05 16:18:40 +0000

    ❄️🛎 Checkpoint ./script.js:383374/1504

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 console.log("if you see this message in the console, then we know this JavaScript file is running!");
 
-// Create a global variable so we can access it in all of our functions
+// Create global variables so we can access them in all of our functions
 let snareDrum;
 let bassDrum;
+let cymbal;
+
 
 // The preload function is required to load things before the page is ready to interact with the user
 function preload() {
   // Load a sound file
   snareDrum = loadSound('https://raw.githubusercontent.com/siggy/beatboxer/master/sounds/snare_drum.wav');
   bassDrum = loadSound('https://raw.githubusercontent.com/siggy/beatboxer/master/sounds/bass_drum.wav');
+  cymbal = loadSound('https://raw.githubusercontent.com/siggy/beatboxer/master/sounds/cymbal.wav');
+  
 }
 
 // The code inside the setup function will run ONLY ONCE when you open the web page
@@ -20,35 +24,39 @@ function setup() {
   frameRate(60);
   
 
-
-  
   ////////////////////////////////////////////////////////////////////////////////////////////////
 }  // end of setup() function
 
 
-// The code inside the draw function will run OVER AND OVER again
-function draw() {
-  // Clear the screen before drawing each next frame
-  clear();
-  
-  
-  if (keyCode === LEFT_ARROW) {
+// This function runs whenever the user types a key on the keyboard:
+function keyTyped() {
+
+  if (key === 'a') {
     // Play the snare drum sound file
     snareDrum.play();
     
-  } else if (keyCode === RIGHT_ARROW) {
+  } else if (key === 'b') {
     
     // Play the bass drum sound file
     bassDrum.play();
     
-  } else if (keyCode === RIGHT_ARROW) {
+  } else if (key === 'c') {
     
-    // Play the bass drum sound file
-    bassDrum.play();
+    // Play the cymbal sound file
+    cymbal.play();
     
   }
   // ..... you can keep chaining more else ifs to play different sounds based on which key the user pressed!
   
+  
+}//end keyPressed() function
+
+
+
+// The code inside the draw function will run OVER AND OVER again
+function draw() {
+
+  // You could also draw something here if you want :)
 
   
 }  // end of draw() function
