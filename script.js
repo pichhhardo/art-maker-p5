244e5056768db3994c522215e3587e1c4fc8ecf7
Glitch (canvas-challenges) <none>
2018-08-05 14:42:38 +0000

    🌿💼 Checkpoint ./script.js:383374/1060

--- script.js
+++ script.js
@@ -26,42 +26,14 @@ function setup() {
 
 
 
+
 // The code inside the draw function will run OVER AND OVER again
 function draw() {
   
-  ////////////////////////////////////////////////////////////////////////////////////////////////
-  // Include your code here inside the draw() function
-  // to draw stuff OVER AND OVER again, creating an animation!
-  ////////////////////////////////////////////////////////////////////////////////////////////////
-  
-  let counter = 0;
 
-  while (  counter < 100   ) {
-
-    let x = random(0, 500);
-    let y = random(0, 500);
-    
-    let w = random(0, 50);
-    let h = random(0, 50);
-    
-    let r = random(0, 500);
-    let g = random(0, 500);
-    let b = random(0, 500);
-    
-    
-    fill(r, g, b);
-    
-    rect(x,y,w,h);
-    
-    
-    counter++;
-  }
   
   
   
-
-  ////////////////////////////////////////////////////////////////////////////////////////////////
-  ////////////////////////////////////////////////////////////////////////////////////////////////
-
+  
 }  // end of draw() function
 
